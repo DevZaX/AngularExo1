382b1827e32df2eaad9fe535e848bf08fdc4ada5
devzax <zakaria_aboud@hotmail.com>
2018-08-08 11:44:22 +0100

    first commit

---
+++ src/app/app-list-item/app-list-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './app-list-item.component.html',
+  styleUrls: ['./app-list-item.component.scss']
+})
+export class AppListItemComponent implements OnInit {
+ 
+ @Input() post:Post
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  love(post){
+  	post.loveIts++
+  }
+
+   dontLove(post){
+  	post.loveIts--
+  }
+
+}
