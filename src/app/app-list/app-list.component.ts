939e052e07034334847c3062ca7e338a23113dff
devzax <zakaria_aboud@hotmail.com>
2018-08-08 12:36:29 +0100

    second commit

--- src/app/app-list/app-list.component.ts
+++ src/app/app-list/app-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
-
+import Post from '../Post'
 
 @Component({
   selector: 'app-list',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Post from './Post'
 
 
 @Component({
@@ -15,16 +16,8 @@ export class AppComponent {
 		new Post('Mon deuxieme Post',"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500"),
 		new Post('Mon troisieme Post',"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500")
 	]
-
-		
 }
 
 
-class Post {
-	constructor(title:string,content:string,loveIts:number,created_at:Date){
-		this.title = title
-		this.content= content,
-		this.loveIts = 0
-		this.created_at = new Date()
-	}
-}
+
+
